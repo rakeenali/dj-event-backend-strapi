a74e9f57e9b3d48e05b919a900f9412b00999dd3
Rakeen Ali <rakeenali20@gmail.com>
2021-11-27 20:23:12 +0500

    This is the entier strapi backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ec5b1c3c398422017d5db1e44372cf99'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '93a2a065-6a6a-42fe-b33c-73e2e9e048b1'
+};
